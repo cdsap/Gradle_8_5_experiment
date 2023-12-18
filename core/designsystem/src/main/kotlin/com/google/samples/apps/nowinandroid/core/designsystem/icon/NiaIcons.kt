a68cfd5f77049686ce190e495ada7de297f4ec56
Inaki Villar <inaki.seri@gmail.com>
2023-12-18 10:32:47 -0800

    no icon

--- core/designsystem/src/main/kotlin/com/google/samples/apps/nowinandroid/core/designsystem/icon/NiaIcons.kt
+++ core/designsystem/src/main/kotlin/com/google/samples/apps/nowinandroid/core/designsystem/icon/NiaIcons.kt
@@ -16,45 +16,26 @@
 
 package com.google.samples.apps.nowinandroid.core.designsystem.icon
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.outlined.Bookmarks
-import androidx.compose.material.icons.outlined.Upcoming
-import androidx.compose.material.icons.rounded.Add
-import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.material.icons.rounded.Bookmark
-import androidx.compose.material.icons.rounded.BookmarkBorder
-import androidx.compose.material.icons.rounded.Bookmarks
-import androidx.compose.material.icons.rounded.Check
-import androidx.compose.material.icons.rounded.Close
-import androidx.compose.material.icons.rounded.Grid3x3
-import androidx.compose.material.icons.rounded.Person
-import androidx.compose.material.icons.rounded.Search
-import androidx.compose.material.icons.rounded.Settings
-import androidx.compose.material.icons.rounded.ShortText
-import androidx.compose.material.icons.rounded.Upcoming
-import androidx.compose.material.icons.rounded.ViewDay
-import androidx.compose.ui.graphics.vector.ImageVector
 
 /**
  * Now in Android icons. Material icons are [ImageVector]s, custom icons are drawable resource IDs.
  */
 object NiaIcons {
-    val Add = Icons.Rounded.Add
-    val ArrowBack = Icons.Rounded.ArrowBack
-    val Bookmark = Icons.Rounded.Bookmark
-    val BookmarkBorder = Icons.Rounded.BookmarkBorder
-    val Bookmarks = Icons.Rounded.Bookmarks
-    val BookmarksBorder = Icons.Outlined.Bookmarks
-    val Check = Icons.Rounded.Check
-    val Close = Icons.Rounded.Close
-    val Grid3x3 = Icons.Rounded.Grid3x3
-    val MoreVert = Icons.Default.MoreVert
-    val Person = Icons.Rounded.Person
-    val Search = Icons.Rounded.Search
-    val Settings = Icons.Rounded.Settings
-    val ShortText = Icons.Rounded.ShortText
-    val Upcoming = Icons.Rounded.Upcoming
-    val UpcomingBorder = Icons.Outlined.Upcoming
-    val ViewDay = Icons.Rounded.ViewDay
+    val Add = Je
+    val ArrowBack = Je
+    val Bookmark = Je
+    val BookmarkBorder = Je
+    val Bookmarks = Je
+    val BookmarksBorder = Je
+    val Check = Je
+    val Close = Je
+    val Grid3x3 = Je
+    val MoreVert = Je
+    val Person = Je
+    val Search = Je
+    val Settings = Je
+    val ShortText = Je
+    val Upcoming = Je
+    val UpcomingBorder = Je
+    val ViewDay = Je
 }
--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -29,7 +29,7 @@ android {
 dependencies {
     api(libs.androidx.compose.foundation)
     api(libs.androidx.compose.foundation.layout)
-    api(libs.androidx.compose.material.iconsExtended)
+    //api(libs.androidx.compose.material.iconsExtended)
     api(libs.androidx.compose.material3)
     api(libs.androidx.compose.runtime)
     api(libs.androidx.compose.runtime.livedata)
